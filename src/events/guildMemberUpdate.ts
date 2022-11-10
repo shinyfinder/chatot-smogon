cf83e45328a8d2ddbc6d18f16e4baad58b0be105
shiny finder <30510670+shinyfinder@users.noreply.github.com>
2022-11-10 18:47:49 -0500

    unboost logging

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -2,11 +2,13 @@ import { AuditLogEvent, GuildMember, EmbedBuilder, User, ChannelType } from 'dis
 import config from '../config';
 import { sleep } from '../helpers/sleep';
 /**
- * Kick handler
+ * Update member handler
  *
- * Event handler for when a user is kicked.
- * Kick events are logged with the user, the mod, and the reason to the specified channel in the config file.
+ * Event handler for when a user is timed out or has their roles changed.
+ * Timeout events are logged with the user, the mod, duration, and the reason to the specified channel in the config file.
  * Content information is supplied by the audit log.
+ *
+ * Boost/Unboosts are logged with the user's name.
  *
  * Note that there is no guarantee when the audit log is populated, nor an event when it is.
  * If the person who deleted the message is a bot or the author, no audit log event is created.
@@ -16,9 +18,9 @@ export = {
     // define the name of the trigger event
     name: 'guildMemberUpdate',
     // execute the code for this event
-    async execute(member: GuildMember) {
+    async execute(oldMember: GuildMember, newMember: GuildMember) {
         // ignore DMs
-        if (!member.guild) {
+        if (!newMember.guild) {
             return;
         }
 
@@ -27,8 +29,41 @@ export = {
             // wait a bit for the audit log to update
             await sleep(10000);
 
+            // for boosting/unboosting, there isn't an audit log entry, so we need to compare the user before and after the change that triggered this event
+            // for main, the boosting role ID is 585582311710654464
+            // check the list of roles assigned to this member for this role id
+            const oldBoost = oldMember.roles.cache.some(role => role.id === '585582311710654464');
+            const newBoost = newMember.roles.cache.some(role => role.id === '585582311710654464');
+
+            // is now boosting
+            if (!oldBoost && newBoost) {
+                /*
+                // get the channel
+                const channel = newMember.client.channels.cache.get(config.LOG_CHANNEL_ID);
+                // make sure you fetched the channel
+                if (channel?.type !== ChannelType.GuildText) {
+                    return;
+                }
+                // send the message
+                await channel.send(`${newMember.user.tag} is now boosting! Their profile is <@${newMember.id}>`);
+                */
+                return;
+            }
+            // no longer boosting :(
+            else if (oldBoost && !newBoost) {
+                // get the channel
+                const channel = newMember.client.channels.cache.get(config.LOG_CHANNEL_ID);
+                // make sure you fetched the channel
+                if (channel?.type !== ChannelType.GuildText) {
+                    return;
+                }
+                // send the message
+                await channel.send(`${newMember.user.tag} is no longer boosting. Their profile is <@${newMember.id}>`);
+                return;
+            }
+
             // since we're only kicking 1 user at a time, fetch the latest event from the audit log of type kick
-            const fetchedLogs = await member.guild.fetchAuditLogs({
+            const fetchedLogs = await newMember.guild.fetchAuditLogs({
                 limit: 1,
                 type: AuditLogEvent.MemberUpdate,
             });
@@ -58,7 +93,7 @@ export = {
             }
 
             // Also run a check to make sure that the log returned was for the same kicked member
-            if (target.id !== member.id) {
+            if (target.id !== newMember.id) {
                 return;
             }
 
@@ -165,11 +200,11 @@ export = {
             let descOut = '';
             if (duration === 'Removed') {
                 titleOut = 'Timeout Removed';
-                descOut = `${member.user.tag}'s timeout was removed by ${executorName}.`;
+                descOut = `${newMember.user.tag}'s timeout was removed by ${executorName}.`;
             }
             else {
                 titleOut = 'User Timed Out';
-                descOut = `${member.user.tag} was timed out by ${executorName}.`;
+                descOut = `${newMember.user.tag} was timed out by ${executorName}.`;
             }
             // build the embed for output
             const embed = new EmbedBuilder()
@@ -177,14 +212,14 @@ export = {
                 .setTitle(`${titleOut}`)
                 .setDescription(`${descOut}`)
                 .addFields(
-                    { name: 'User', value: `<@${member.id}>`, inline: true },
+                    { name: 'User', value: `<@${newMember.id}>`, inline: true },
                     { name: 'Duration', value: `${duration}`, inline: true },
                     { name: 'Performed by', value: `${executorOut}` },
                     { name: 'Reason', value: `${reason}` },
                 );
 
             // log to the logging channel specified in the config file
-            const channel = member.client.channels.cache.get(config.LOG_CHANNEL_ID);
+            const channel = newMember.client.channels.cache.get(config.LOG_CHANNEL_ID);
             if (channel?.type !== ChannelType.GuildText) {
                 return;
             }
