5a857b5c53a68b85aadae58b08ee8ebf3df8a7c9
sf <30510670+shinyfinder@users.noreply.github.com>
2023-09-11 13:50:12 -0400

    add search ban support

--- src/commands/find.ts
+++ src/commands/find.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, PermissionFlagsBits, GuildMember, Collection } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, PermissionFlagsBits, GuildMember, Collection, SlashCommandSubcommandBuilder, GuildBan } from 'discord.js';
 import { SlashCommand } from '../types/slash-command-base';
 
 /**
@@ -12,11 +12,21 @@ export const command: SlashCommand = {
     // setup the slash command builder
     data: new SlashCommandBuilder()
         .setName('find')
-        .setDescription('Retrieves a user\'s profile given their Discord display-, nick-, or username')
-        .addStringOption(option =>
-            option.setName('name')
-            .setDescription('Discord display name, nickname, or username')
-            .setRequired(true))
+        .setDescription('Attempts to locate the searched item')
+        .addSubcommand(new SlashCommandSubcommandBuilder()
+            .setName('user')
+            .setDescription('Retrieves a user\'s profile given their Discord display-, nick-, or username')
+            .addStringOption(option =>
+                option.setName('name')
+                .setDescription('Discord display name, nickname, or username')
+                .setRequired(true)))
+        .addSubcommand(new SlashCommandSubcommandBuilder()
+            .setName('ban')
+            .setDescription('Finds the ban in the server with the provided info')
+            .addStringOption(option =>
+                option.setName('search')
+                .setDescription('Username, display name, user id, or ban reason')
+                .setRequired(true)))
         .setDMPermission(false)
         .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
 
@@ -29,57 +39,96 @@ export const command: SlashCommand = {
         }
         await interaction.deferReply({ ephemeral: true });
 
-        // get the user input
-        const username = interaction.options.getString('name', true).toLowerCase();
+        if (interaction.options.getSubcommand() === 'user') {
+            // get the user input
+            const username = interaction.options.getString('name', true).toLowerCase();
 
-        // holder for each member found that matches the entered name
-        let foundMembers: Collection<string, GuildMember> = new Collection;
+            // holder for each member found that matches the entered name
+            let foundMembers: Collection<string, GuildMember> = new Collection;
 
-        // check the current guild first to get the most relevant info
-        const currentMembers = await interaction.guild.members.fetch();
-        const currentFound = currentMembers.filter(member => member.nickname?.toLowerCase() === username || member.user.username === username || member.user.displayName.toLowerCase() === username);
-        // if you got a match, concat it into the holder
-        if (currentFound) {
-            foundMembers = foundMembers.merge(currentFound, oldFound => ({ keep: true, value: oldFound }), newFound => ({ keep: true, value: newFound }), () => ({ keep: false }));
-        }
-
-        // loop over the guilds the bot is in, looking for someone with that username
-        for (const guild of interaction.client.guilds.cache.values()) {
-            // skip rechecking the current server
-            if (guild.id === interaction.guildId) {
-                continue;
+            // check the current guild first to get the most relevant info
+            const currentMembers = await interaction.guild.members.fetch();
+            const currentFound = currentMembers.filter(member => member.nickname?.toLowerCase() === username || member.user.username === username || member.user.displayName.toLowerCase() === username);
+            // if you got a match, concat it into the holder
+            if (currentFound) {
+                foundMembers = foundMembers.merge(currentFound, oldFound => ({ keep: true, value: oldFound }), newFound => ({ keep: true, value: newFound }), () => ({ keep: false }));
             }
-            // fetch the guild member list
-            const memberList = await guild.members.fetch();
 
-            // filter out the users we care about
-            const matchingMembers = memberList.filter(member => member.nickname?.toLowerCase() === username || member.user.username === username || member.user.displayName.toLowerCase() === username);
-            
-            // if you found a match, add it to the holder
-            // keep the first found result so we don't duplicate users
-            if (matchingMembers) {
-                foundMembers = foundMembers.merge(matchingMembers, oldFound => ({ keep: true, value: oldFound }), newFound => ({ keep: true, value: newFound }), (oldFound) => ({ keep: true, value: oldFound }));
+            // loop over the guilds the bot is in, looking for someone with that username
+            for (const guild of interaction.client.guilds.cache.values()) {
+                // skip rechecking the current server
+                if (guild.id === interaction.guildId) {
+                    continue;
+                }
+                // fetch the guild member list
+                const memberList = await guild.members.fetch();
+
+                // filter out the users we care about
+                const matchingMembers = memberList.filter(member => member.nickname?.toLowerCase() === username || member.user.username === username || member.user.displayName.toLowerCase() === username);
+                
+                // if you found a match, add it to the holder
+                // keep the first found result so we don't duplicate users
+                if (matchingMembers) {
+                    foundMembers = foundMembers.merge(matchingMembers, oldFound => ({ keep: true, value: oldFound }), newFound => ({ keep: true, value: newFound }), (oldFound) => ({ keep: true, value: oldFound }));
+                }
+                else {
+                    continue;
+                }
+
+            }
+
+            // get the unique members
+            let strOut = '';
+            for (const mem of foundMembers.values()) {
+                strOut += `${mem.displayName} (${mem.user.username}): ${mem.toString()}\n`;
+            }
+
+            // alert with results
+            if (strOut.length) {
+                await interaction.followUp(strOut);
             }
             else {
-                continue;
+                await interaction.followUp('User not found in any shared servers');
+            }
+        }
+        else if (interaction.options.getSubcommand() === 'ban') {
+            // get the user input
+            const str = interaction.options.getString('search', true).toLowerCase();
+
+            // get the collection of bans in the server
+            // discord returns a max of 1000 at a time
+            // so we need to build the entire collection via iteration
+            let numReturned = 1000;
+            let banlist: Collection<string, GuildBan> = new Collection;
+            let lastBan: string | undefined = '0';
+
+            while (numReturned === 1000) {
+                // retieve up to 1000 bans from the api
+                const bans: Collection<string, GuildBan> = await interaction.guild.bans.fetch({ after: lastBan, limit: 1000 });
+
+                // get the user id of the last ban for pagination
+                lastBan = bans.last()?.user.id;
+
+                // get the number of bans returned to break the loop
+                // if this number is < 1000, there are no more bans to be retrieved
+                numReturned = bans.size;
+
+                // concat the retrieved collection with the old one
+                banlist = banlist.merge(bans, oldFound => ({ keep: true, value: oldFound }), newFound => ({ keep: true, value: newFound }), () => ({ keep: false }));
             }
 
-        }
+            // filter the built collection for the value entered
+            const bannedMatches = banlist.filter(ban => ban.user.username.includes(str) || ban.user.displayName.toLowerCase().includes(str) || ban.user.id === str || ban.reason?.toLowerCase().includes(str));
 
-        // get the unique members
-        let strOut = '';
-        for (const mem of foundMembers.values()) {
-            strOut += `${mem.displayName} (${mem.user.username}): ${mem.toString()}\n`;
+            // if you found a match, output the ids so that they can enter the id into the ban menu
+            if (bannedMatches.size) {
+                const bannedIDs = bannedMatches.map(ban => ban.user.id);
+                await interaction.followUp(`Matching bans found. Please enter the following user ID(s) into the Discord bans menu to find the ban: ${bannedIDs.join(', ')}`);
+            }
+            // otherwise, let them know nothing was found
+            else {
+                await interaction.followUp('No bans found with that criteria.');
+            }
         }
-
-        // alert with results
-        if (strOut.length) {
-            await interaction.followUp(strOut);
-        }
-        else {
-            await interaction.followUp('User not found in any shared servers');
-        }
-        
-
     },
 };
