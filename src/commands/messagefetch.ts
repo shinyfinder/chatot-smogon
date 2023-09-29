00279b83edcb44bfb85339876acd138f1486e149
sf <30510670+shinyfinder@users.noreply.github.com>
2023-09-29 15:24:12 -0400

    add messagefetch

---
+++ src/commands/messagefetch.ts
@@ -0,0 +1,36 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, PermissionFlagsBits, ChannelType } from 'discord.js';
+import { SlashCommand } from '../types/slash-command-base';
+
+/**
+ * Deletes messages in bulk from the specified channel
+ * This is currently a dev-only command
+ * Messages must be newer than 2 weeks
+ */
+
+export const command: SlashCommand = {
+    global: false,
+    guilds: [],
+    // setup the slash command builder
+    data: new SlashCommandBuilder()
+        .setName('messagefetch')
+        .setDescription('Fetches a message from the interaction channel')
+        .addStringOption(option =>
+            option.setName('messageid')
+            .setDescription('The message to fetch')
+            .setRequired(true))
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+    // execute our desired task
+    async execute(interaction: ChatInputCommandInteraction) {
+        await interaction.deferReply({ ephemeral: true });
+        // get the user input
+        const id = interaction.options.getString('messageid', true);
+        
+        const msg = await interaction.channel?.messages.fetch(id);
+
+        // done
+        await interaction.followUp('Done');
+    },
+
+};
