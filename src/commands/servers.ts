026b4e93d69044ac2726863ca30e71d8f536d6fc
sf <30510670+shinyfinder@users.noreply.github.com>
2024-03-09 14:56:45 -0500

    add autocomplete to servers

--- src/commands/servers.ts
+++ src/commands/servers.ts
@@ -1,6 +1,7 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, PermissionFlagsBits } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, PermissionFlagsBits, AutocompleteInteraction } from 'discord.js';
 import { SlashCommand } from '../types/slash-command-base';
 import { buildEmbed, embedField } from '../helpers/logging.js';
+import { filterAutocomplete } from '../helpers/autocomplete.js';
 /**
  * Prints a list of servers the bot is in,
  * or the details of the specified server.
@@ -16,9 +17,22 @@ export const command: SlashCommand = {
         .addStringOption(option =>
             option.setName('name')
             .setDescription('Name (or id) of the server to query. Bot must be in it')
-            .setRequired(false))
+            .setRequired(false)
+            .setAutocomplete(true))
         .setDMPermission(false)
         .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+
+    // prompt the user with autocomplete options since there are too many tiers to have a selectable list
+    async autocomplete(interaction: AutocompleteInteraction) {
+        const focusedOption = interaction.options.getFocused(true);
+
+        if (focusedOption.name === 'name') {
+            const guildNamesIDs = interaction.client.guilds.cache.map(guild => ({ name: guild.name, id: guild.id }));
+            const guildPairs = guildNamesIDs.map(g => ({ name: g.name, value: g.id }));
+
+            await filterAutocomplete(interaction, focusedOption, guildPairs);
+        }
+    },
 
     // execute our desired task
     async execute(interaction: ChatInputCommandInteraction) {
@@ -47,7 +61,7 @@ export const command: SlashCommand = {
 
                     // build an embed
                     const title = 'Server Details';
-                    const desc = `Showing details for server ${guildQ}`;
+                    const desc = `Showing details for server ${guild.name}`;
                     const fields: embedField[] = [
                         { name: 'Name', value: guild.name, inline: true },
                         { name: 'ID', value: guild.id, inline: true },
