b60bcc0bd62c50d0ffeb1df98ac7aaf68ff7ce18
sf <30510670+shinyfinder@users.noreply.github.com>
2024-03-20 17:18:12 -0400

    add functionality to modpost edit

--- src/commands/modpost.ts
+++ src/commands/modpost.ts
@@ -10,11 +10,12 @@ import {
     ActionRowBuilder,
     ModalActionRowComponentBuilder,
     ModalSubmitInteraction,
+    Message,
  } from 'discord.js';
 import { SlashCommand } from '../types/slash-command-base';
-import { pool } from '../helpers/createPool.js';
 import { getRandInt } from '../helpers/getRandInt.js';
 import { checkChanPerms } from '../helpers/checkChanPerms.js';
+import { botConfig } from '../config.js';
 /**
  * Posts a message to the specific channel with the provided content.
  * Also allows the post to be edited.
@@ -80,6 +81,7 @@ export const command: SlashCommand = {
                 .setCustomId(`modpost-text-${randInt}`)
                 .setLabel('Message')
                 .setStyle(TextInputStyle.Paragraph)
+                .setMaxLength(1925)
                 .setRequired(true);
 
             // add an action row to hold the text inputs
@@ -98,13 +100,23 @@ export const command: SlashCommand = {
             // wait for them to submit the modal
             const submittedModal = await interaction.awaitModalSubmit({ filter, time: 5 * 60 * 1000 });
 
-            const modMsg = submittedModal.fields.getTextInputValue(`modpost-text-${randInt}`);
+            // get what they entered
+            let modMsg = submittedModal.fields.getTextInputValue(`modpost-text-${randInt}`);
+
+            // get the unix time
+            const dateUnix = Math.floor(Date.now() / 1000);
+
+            // construct the object needed for discord
+            const cordTime = `<t:${dateUnix}:f>`;
+
+            // sign the message
+            modMsg += `\n\nLast modpost edit: ${interaction.user.username} ${cordTime}`;
 
             // respond with their message
             await chan.send(modMsg);
 
             // done
-            await interaction.followUp({ content: 'Post made', ephemeral: true });
+            await submittedModal.reply({ content: 'Post made', ephemeral: true });
         }
 
         /**
@@ -112,37 +124,88 @@ export const command: SlashCommand = {
          */
 
         if (interaction.options.getSubcommand() === 'edit') {
-            /*
+            
             // get the info they entered
             const url = interaction.options.getString('url', true);
 
             // split the url
-
-            // make sure we have the necessary perms to post there
-            let canComplete = true;
-            if (chan.type === ChannelType.PublicThread) {
-                canComplete = await checkChanPerms(interaction, chan, ['ViewChannel', 'SendMessagesInThreads']);
+            // goes discord.com/channels/{server id}/{channel id}/{message id}
+            const idMatchArr = url.match(/\d+/g);
+            
+            // validate the input
+            if (!idMatchArr) {
+                await interaction.reply({ content: 'Invalid input. Please proivde the link to the message.', ephemeral: true });
+                return;
             }
-            else if (chan.type === ChannelType.GuildText) {
-                canComplete = await checkChanPerms(interaction, chan, ['ViewChannel', 'SendMessages']);
+            else if (idMatchArr.length !== 3) {
+                await interaction.reply({ content: 'Did you give me the right link? Please provide a link to the message, not just the ID of the message.', ephemeral: true });
+                return;
             }
+            else if (idMatchArr[0] !== interaction.guildId) {
+                await interaction.reply({ content: 'The message must be in this server', ephemeral: true });
+                return;
+            }
+            
+            // try to fetch the channel
+            const chan = interaction.client.channels.cache.get(idMatchArr[1]);
 
-            if (!canComplete) {
+            if (!chan) {
+                await interaction.reply({ content: 'Invalid channel id; I don\'t recognize that channel', ephemeral: true });
+                return;
+            }
+            else if (chan.type !== ChannelType.GuildText) {
+                await interaction.reply({ content: 'Invalid channel id; I don\'t recognize that channel', ephemeral: true });
+                return;
+            }
+           
+            if (chan.guildId !== interaction.guildId) {
+                await interaction.reply({ content: 'Channel must be in this server!', ephemeral: true });
                 return;
             }
 
+            // fetch the mesage
+            let msg: Message;
+            try {
+                msg = await chan.messages.fetch(idMatchArr[2]);
+            }
+            catch (e) {
+                // the only way this will throw is if they somehow mistype the message id or pass a private channel we don't have access to anymore
+                // don't bother passing through the error handler because we wouldn't need to log this error anyway
+                await interaction.reply({ content: 'Unknown message. Did you mistype the URL? Do I still have access to that channel?', ephemeral: true });
+                return;
+            }
+
+            // make sure we're the author
+            if (msg.author.id !== botConfig.CLIENT_ID) {
+                await interaction.reply({ content: 'I can only edit messages I am the author of', ephemeral: true });
+                return;
+            }
+            // make sure it includes the signature so they don't pass a random message of ours
+            else if (!/Last modpost edit: /.test(msg.content)) {
+                await interaction.reply({ content: 'I can only edit messages made via the modpost command', ephemeral: true });
+                return;
+            }
+
+            // at this point we should (finally) have validated the use case
+            // before we can show them the modal, strip the signature line
+            const unsignedText = msg.content.replace(/\n\nLast modpost edit:.*/, '');
+
+            // now show them the modal
+            
             // get rand int to uniquely id the modal
             const randInt = getRandInt(0, 65535);
 
             // instantiate a modal for user input
             const modal = new ModalBuilder()
-                .setCustomId(`modpost${randInt}`)
-                .setTitle('New Mod Post');
+                .setCustomId(`modpost-edit-${randInt}`)
+                .setTitle('Edit Mod Post');
 
             const msgTextInput = new TextInputBuilder()
-                .setCustomId(`modpost-text-${randInt}`)
+                .setCustomId(`modpost-edit-text-${randInt}`)
                 .setLabel('Message')
                 .setStyle(TextInputStyle.Paragraph)
+                .setMaxLength(1925)
+                .setValue(unsignedText)
                 .setRequired(true);
 
             // add an action row to hold the text inputs
@@ -156,20 +219,29 @@ export const command: SlashCommand = {
             await interaction.showModal(modal);
             
             // await their input
-            const filter = (modalInteraction: ModalSubmitInteraction) => modalInteraction.customId === `modpost${randInt}` && modalInteraction.user.id === interaction.user.id;
+            const filter = (modalInteraction: ModalSubmitInteraction) => modalInteraction.customId === `modpost-edit-${randInt}` && modalInteraction.user.id === interaction.user.id;
             
             // wait for them to submit the modal
             const submittedModal = await interaction.awaitModalSubmit({ filter, time: 5 * 60 * 1000 });
 
-            const modMsg = submittedModal.fields.getTextInputValue(`modpost-text-${randInt}`);
+            // get what they entered
+            let modMsg = submittedModal.fields.getTextInputValue(`modpost-edit-text-${randInt}`);
+
+            // get the unix time
+            const dateUnix = Math.floor(Date.now() / 1000);
+
+            // construct the object needed for discord
+            const cordTime = `<t:${dateUnix}:f>`;
+
+            // sign the message
+            modMsg += `\n\nLast modpost edit: ${interaction.user.username} ${cordTime}`;
 
             // respond with their message
-            await chan.send(modMsg);
+            await msg.edit(modMsg);
 
             // done
-            await interaction.followUp({ content: 'Post made', ephemeral: true });
-            */
-            await interaction.followUp('wip');
+            await submittedModal.reply({ content: 'Post edited', ephemeral: true });
+            
         }
         
     },
--- src/helpers/checkChanPerms.ts
+++ src/helpers/checkChanPerms.ts
@@ -1,10 +1,10 @@
+/* eslint-disable @typescript-eslint/no-base-to-string */
 import { CommandInteraction, GuildChannelResolvable, ModalSubmitInteraction, PermissionResolvable } from 'discord.js';
 
 /**
  * Checks channel permissions for the ability to complete the command. 
  * If the bot lacks the permissions, it will respond with the necessary ones.
  * 
- * Interactions should be deferred.
  * @param interaction Slash/Context menu interaction
  * @param perms Array of PermissionResolvable
  * @returns Promise<boolean>
@@ -23,8 +23,15 @@ export async function checkChanPerms(interaction: CommandInteraction | ModalSubm
 
     // make sure we have the permissions we need
     if (!chanPerms.has(perms)) {
-        // eslint-disable-next-line @typescript-eslint/no-base-to-string
-        await interaction.followUp(`I lack the permissions to invoke this command. Please ensure I have the following permissions in ${channel.toString()}, then start over: ${perms.join(', ')}`);
+        // if we don't, reply with the permissions we need
+        // try to make this ephemeral since it's an error message
+        if (interaction.replied) {
+            await interaction.followUp(`I lack the permissions to invoke this command. Please ensure I have the following permissions in ${channel.toString()}, then start over: ${perms.join(', ')}`);
+        }
+        else {
+            await interaction.reply({ content: `I lack the permissions to invoke this command. Please ensure I have the following permissions in ${channel.toString()}, then start over: ${perms.join(', ')}`, ephemeral: true });
+        }
+        
         return false;
     }
 
