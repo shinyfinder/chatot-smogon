885caef92047bf907f25bf14ad8acc29cc559645
sf <30510670+shinyfinder@users.noreply.github.com>
2024-04-18 13:08:26 -0400

    catch error for unknown guild

--- src/helpers/markOfficial.ts
+++ src/helpers/markOfficial.ts
@@ -1,4 +1,4 @@
-import { ButtonInteraction } from 'discord.js';
+import { ButtonInteraction, DiscordAPIError } from 'discord.js';
 import { pool } from './createPool.js';
 import { ServerClass } from './constants.js';
 /**
@@ -23,28 +23,36 @@ export async function markOfficial(interaction: ButtonInteraction) {
     // id of new guild that was added
     const guildID = splitCustomID[0];
 
-    // get the guild name for better reporting
-    const guildName = (await interaction.client.guilds.fetch(guildID)).name;
+    try {
+        // get the guild name for better reporting
+        const guildName = (await interaction.client.guilds.fetch(guildID)).name;
 
-    // whether we care or not
-    const action = splitCustomID[splitCustomID.length - 1];
+        // whether we care or not
+        const action = splitCustomID[splitCustomID.length - 1];
 
-    if (action === 'confirm') {
-        // insert into the db
-        await pool.query(`
-        INSERT INTO chatot.servers (serverid, class)
-        VALUES ($1, $2)
-        ON CONFLICT (serverid) DO
-        UPDATE SET class = EXCLUDED.class`, [guildID, ServerClass.Official]);
+        if (action === 'confirm') {
+            // insert into the db
+            await pool.query(`
+            INSERT INTO chatot.servers (serverid, class)
+            VALUES ($1, $2)
+            ON CONFLICT (serverid) DO
+            UPDATE SET class = EXCLUDED.class`, [guildID, ServerClass.Official]);
 
-        // update log message
-        await interaction.message.edit({ content: `Ok, I will try to gban in ${guildName} (${guildID}) from now on. If I have any issues, I'll let you know.`, components: [] });
+            // update log message
+            await interaction.message.edit({ content: `Ok, I will try to gban in ${guildName} (${guildID}) from now on. If I have any issues, I'll let you know.`, components: [] });
+        }
+        else if (action === 'deny') {
+            // update log message
+            await interaction.message.edit({ content: `Ok, I'll ignore gban errors in ${guildName} (${guildID}).`, components: [] });
+        }
     }
-    else if (action === 'deny') {
-        // update log message
-        await interaction.message.edit({ content: `Ok, I'll ignore gban errors in ${guildName} (${guildID}).`, components: [] });
+    catch (e) {
+        if (e instanceof DiscordAPIError && e.message.includes('Unknown Guild')) {
+            await interaction.message.edit({ content: 'I\'m no longer in this guild, nothing to do.', components: [] });
+        }
+        else {
+            throw e;
+        }
     }
-    else {
-        return;
-    }
+    
 }
